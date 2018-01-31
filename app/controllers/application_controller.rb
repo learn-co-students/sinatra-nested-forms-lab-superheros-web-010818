620ca8c6cb5e19c6a3c59f87e267a3155ea8d273
Anna Yang <yanganna68@gmail.com>
2018-01-31 16:37:57 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
+
+  post '/teams' do
+    # binding.pry
+    team = params["team"]
+    members = team["members"]
+    @team = Team.new(team["name"], team["motto"])
+    @hero1 = Hero.new(members[0]["name"],members[0]["power"],members[0]["bio"])
+    @hero2 = Hero.new(members[1]["name"],members[1]["power"],members[1]["bio"])
+    @hero3 = Hero.new(members[2]["name"],members[2]["power"],members[2]["bio"])
+    erb :team
+
+  end
 
 
 end
---
+++ app/models/hero.rb
@@ -0,0 +1,8 @@
+class Hero
+  attr_accessor :name, :power, :bio
+  def initialize(name, power, bio)
+    @name = name
+    @power = power
+    @bio = bio
+  end
+end
