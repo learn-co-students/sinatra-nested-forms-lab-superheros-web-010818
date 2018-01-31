ab49aacd7fd8d69c8463f5bed2fb526b3c7750a3
GuttermanA <alexanderf.gutterman@gmail.com>
2018-01-31 10:44:20 -0500

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,12 @@
+class Hero
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(name, power, bio)
+    @name = name
+    @power = power
+    @bio = bio
+    @@all << self
+  end
+end
